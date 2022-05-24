e021e002c2a3dbcbbb0eb6acbae0ca5242e38405
Jim Turner <jaxbeachcft@gmail.com>
2022-05-24 17:05:57 -0400

    commit4

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config():
 
   FLASK_APP = os.getenv('FLASK_APP')
   FLASK_ENV = os.getenv('FLASK_ENV')
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+  SECRET_KEY = os.environ.get('MY_SECRET_KEY')
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_NOTIFICATIONS = False
 
